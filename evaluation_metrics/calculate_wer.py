aeb07c783f7d73d85f627d422576607dc04ecc41
kohei0209 <7wg114562ssk@toki.waseda.jp>
2024-11-15 01:25:18 +0900

    support cer

--- evaluation_metrics/calculate_wer.py
+++ evaluation_metrics/calculate_wer.py
@@ -1,20 +1,17 @@
+import json
 from pathlib import Path
 
-import json
 import numpy as np
 import soundfile as sf
 import soxr
 import torch
-from Levenshtein import opcodes
-from tqdm import tqdm
-
-from owsm_utils import owsm_predict
-
 from espnet2.bin.s2t_inference import Speech2Text
 from espnet2.text.cleaner import TextCleaner
+from Levenshtein import opcodes
+from owsm_utils import owsm_predict
+from tqdm import tqdm
 
-
-METRICS = ("WER",)
+METRICS = ("WER", "CER")
 TARGET_FS = 16000
 BEAMSIZE = 5
 
@@ -22,7 +19,7 @@ BEAMSIZE = 5
 ################################################################
 # Definition of metrics
 ################################################################
-def levenshtein_metric(model, textcleaner, ref_txt, inf, fs=16000):
+def levenshtein_metric(model, textcleaner, ref_txt, inf, lang_id, fs=16000):
     """Calculate the Levenshtein distance between ref and inf ASR results.
 
     Args:
@@ -43,21 +40,21 @@ def levenshtein_metric(model, textcleaner, ref_txt, inf, fs=16000):
     if fs != TARGET_FS:
         inf = soxr.resample(inf, fs, TARGET_FS)
         fs = TARGET_FS
+
     with torch.no_grad():
         inf_txt = owsm_predict(
             model,
             inf,
             fs,
-            src_lang="eng",
+            src_lang=lang_id,
             beam_size=BEAMSIZE,
             long_form=len(inf) > 30 * fs,
         )
-
     ref_txt = textcleaner(ref_txt)
     inf_txt = textcleaner(inf_txt)
     ref_words = ref_txt.split()
     inf_words = inf_txt.split()
-    ret = {
+    ret_wer = {
         "hyp_text": inf_txt,
         "ref_text": ref_txt,
         "delete": 0,
@@ -67,13 +64,36 @@ def levenshtein_metric(model, textcleaner, ref_txt, inf, fs=16000):
     }
     for op, ref_st, ref_et, inf_st, inf_et in opcodes(ref_words, inf_words):
         if op == "insert":
-            ret[op] = ret[op] + inf_et - inf_st
+            ret_wer[op] = ret_wer[op] + inf_et - inf_st
         else:
-            ret[op] = ret[op] + ref_et - ref_st
-    total = ret["delete"] + ret["replace"] + ret["equal"]
+            ret_wer[op] = ret_wer[op] + ref_et - ref_st
+    total = ret_wer["delete"] + ret_wer["replace"] + ret_wer["equal"]
     assert total == len(ref_words), (total, len(ref_words))
-    total = ret["insert"] + ret["replace"] + ret["equal"]
+    total = ret_wer["insert"] + ret_wer["replace"] + ret_wer["equal"]
     assert total == len(inf_words), (total, len(inf_words))
+
+    # process cer
+    ref_words = [c for c in ref_txt]
+    inf_words = [c for c in inf_txt]
+    ret_cer = {
+        "hyp_text": inf_words,
+        "ref_text": ref_words,
+        "delete": 0,
+        "insert": 0,
+        "replace": 0,
+        "equal": 0,
+    }
+    for op, ref_st, ref_et, inf_st, inf_et in opcodes(ref_words, inf_words):
+        if op == "insert":
+            ret_cer[op] = ret_cer[op] + inf_et - inf_st
+        else:
+            ret_cer[op] = ret_cer[op] + ref_et - ref_st
+    total = ret_cer["delete"] + ret_cer["replace"] + ret_cer["equal"]
+    assert total == len(ref_words), (total, len(ref_words))
+    total = ret_cer["insert"] + ret_cer["replace"] + ret_cer["equal"]
+    assert total == len(inf_words), (total, len(inf_words))
+
+    ret = dict(WER=ret_wer, CER=ret_cer)
     return ret
 
 
@@ -82,7 +102,9 @@ def levenshtein_metric(model, textcleaner, ref_txt, inf, fs=16000):
 ################################################################
 def main(args):
     transcripts = {}
+    language_id = {}
     if args.meta_tsv.endswith(".tsv"):
+        assert False, "tsv is not supported now"
         with open(args.meta_tsv, "r") as f:
             headers = next(f).strip().split("\t")
             uid_idx = headers.index("id")
@@ -98,11 +120,17 @@ def main(args):
                 uid, txt = line.strip().split(maxsplit=1)
                 transcripts[uid] = txt
 
+        with open(args.utt2lang, "r") as f:
+            for line in f:
+                uid, lang_id = line.strip().split(maxsplit=1)
+                assert uid in transcripts, uid
+                language_id[uid] = lang_id
+
     data_pairs = []
     with open(args.inf_scp, "r") as f:
         for line in f:
             uid, audio_path = line.strip().split()
-            data_pairs.append((uid, transcripts[uid], audio_path))
+            data_pairs.append((uid, transcripts[uid], audio_path, language_id[uid]))
 
     size = len(data_pairs)
     assert 1 <= args.job <= args.nsplits <= size
@@ -125,20 +153,22 @@ def main(args):
     model = Speech2Text.from_pretrained(
         model_tag="espnet/owsm_v3.1_ebf",
         device=args.device,
-        lang_sym="<eng>",
+        # lang_sym="<eng>",
         task_sym="<asr>",
         beam_size=BEAMSIZE,
         predict_time=False,
     )
-    textcleaner = TextCleaner("whisper_en")
+    textcleaner = TextCleaner("whisper_basic")
+    # textcleaner = TextCleaner("whisper_en")
     ret = []
-    for uid, ref_text, inf_audio in tqdm(data_pairs):
+    for uid, ref_text, inf_audio, lang_id in tqdm(data_pairs):
         _, score = process_one_pair(
-            (uid, ref_text, inf_audio), model=model, textcleaner=textcleaner
+            (uid, ref_text, inf_audio, lang_id), model=model, textcleaner=textcleaner
         )
         ret.append((uid, score))
         for metric, value in score.items():
-            if metric == "WER":
+            # if metric == "WER":
+            if metric in ["WER", "CER"]:
                 s = json.dumps(value)
                 writers[metric].write(f"{uid} {s}\n")
 
@@ -148,7 +178,8 @@ def main(args):
     if args.nsplits == args.job == 1:
         with (outdir / "RESULTS.txt").open("w") as f:
             for metric in METRICS:
-                if metric == "WER":
+                # if metric == "WER":
+                if metric in ["WER", "CER"]:
                     dic = {"delete": [], "insert": [], "replace": [], "equal": []}
                     for uid, score in ret:
                         if len(score[metric]) == 0:
@@ -171,14 +202,16 @@ def main(args):
 
 
 def process_one_pair(data_pair, model=None, textcleaner=None):
-    uid, ref_txt, inf_path = data_pair
+    uid, ref_txt, inf_path, lang_id = data_pair
     inf, fs = sf.read(inf_path, dtype="float32")
     assert inf.ndim == 1, inf.shape
 
     scores = {}
+    ret = levenshtein_metric(model, textcleaner, ref_txt, inf, lang_id, fs=fs)
     for metric in METRICS:
-        if metric == "WER":
-            scores[metric] = levenshtein_metric(model, textcleaner, ref_txt, inf, fs=fs)
+        if metric in ["WER", "CER"]:
+            # scores[metric] = levenshtein_metric(model, textcleaner, ref_txt, inf, lang_id, fs=fs)
+            scores[metric] = ret[metric]
         else:
             raise NotImplementedError(metric)
 
@@ -202,6 +235,12 @@ if __name__ == "__main__":
         type=str,
         required=True,
         help="Path to the scp file containing enhanced signals",
+    )
+    parser.add_argument(
+        "--utt2lang",
+        type=str,
+        required=True,
+        help="Path to the scp file containing language id",
     )
     parser.add_argument(
         "--output_dir",
